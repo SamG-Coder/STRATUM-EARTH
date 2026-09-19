8cd353769bafc430623f5c7c837a981ea1153d3c
Sam <13449963+SamG-Coder@users.noreply.github.com>
2026-09-19 16:45:45 +1000

    Use specialized compiler options in worker

--- src/compiler-worker.js
+++ src/compiler-worker.js
@@ -1,2 +1,2 @@
-import {compile} from '../vendor/cuda-webshader/compiler/compiler.js';
-self.onmessage=event=>{const {id,source,spec}=event.data;try{const {ast,kernel,...artifact}=compile(source,{entry:spec.entry,workgroupSize:spec.workgroupSize});self.postMessage({id,artifact});}catch(e){self.postMessage({id,error:String(e.stack||e)});}};
+import {compile} from '../vendor/cuda-webshader/compiler/compiler.js';\nimport {compilerOptions} from './kernel-specs.js';
+self.onmessage=event=>{const {id,source,spec}=event.data;try{const {ast,kernel,...artifact}=compile(source,compilerOptions(spec));self.postMessage({id,artifact});}catch(e){self.postMessage({id,error:String(e.stack||e)});}};
